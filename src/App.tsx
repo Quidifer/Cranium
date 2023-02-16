75f33e4dd110dcf10dfa0b291c630d2bac88dd73
christopher mouri <christophermouri@gmail.com>
2023-02-16 15:17:06 -0800

    added eslint and prettier dev dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const variable = "hello world";
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload testing.
-        </p>
+        <p>{variable}.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
