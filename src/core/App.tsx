e859a03887ca64ce66f2e9a75e3dd30305f60022
ryandalmas <ryan.j.dalmas@gmail.com>
2023-02-19 11:08:34 -0800

    Added File structure, CraniumButton component

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import CraniumButton from ".././components/ui/CraniumButton";
 
 function App() {
   const variable = "hello world";
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <CraniumButton>Finish</CraniumButton>
       </header>
     </div>
   );
---
+++ src/utils/ColorPallet.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ColorPallet = {
+    White: '#FFFFFF',
+    Black: '#000000',
+
+    LightGreen: '#52cc00',
+    Red: '#cc0000',
+
+}
+
+export default ColorPallet;
