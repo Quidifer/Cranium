e87ea6a9009d2d3410b5875ee0895ff9738c272b
Tran Nguyen <tran28nguyen@gmail.com>
2023-03-12 21:29:30 -0700

    made component grid

--- client/src/components/ui/Load/load.tsx
+++ client/src/components/ui/Load/load.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import "./load.css";
 // import UploadManifest from '../UploadManifest/uploadManifest';
 // import { ContextMenu, MenuContextContainer } from '../ContextMenu/contextMenu';
+import Grid from '../Grid/grid';
 
 interface Props {
   setScreenState: React.Dispatch<React.SetStateAction<string>>;
@@ -11,8 +12,8 @@ interface Props {
 
 export default function Load(props: Props) {
   const { manifest } = props;
-  const [ selectedCells, setSelectedCells ] = useState([{ row: 0, col: 0 }]);
-  const [ selectedNames, setSelectedNames ] = useState<string[]>([]);
+  // const [ selectedCells, setSelectedCells ] = useState([{ row: 0, col: 0 }]);
+  // const [ selectedNames, setSelectedNames ] = useState<string[]>([]);
   const [ counts, setCounts ] = useState<Record<string, number>[]>([]);
   const [ isGridSelectable, setIsGridSelectable ] = useState(true);
   // const [ clicked, setClicked ] = useState(false);
@@ -26,121 +27,129 @@ export default function Load(props: Props) {
   //   };
   // }, []);
 
-  const isInList = (list:any, row: any, col: any) => {
-    return list.some((item: any) => item.row === row && item.col === col)
-  }
+  // const isInList = (list:any, row: any, col: any) => {
+  //   return list.some((item: any) => item.row === row && item.col === col)
+  // }
 
-  const Tile = (props: any) => {
-    const { color, id, row, col, name } = props;
-    return (
-      <div
-        id={id}
-        className={isGridSelectable ? `${color} tile` : `nan tile`}
-        onMouseDown={() => {
-            if (isGridSelectable && isInList(manifest, row, col) && name !== "NAN" && name !== "UNUSED") {
-              if (isInList(selectedCells, row, col)) { // deselect cell
-                setSelectedCells(selectedCells.filter(item => item.row !== row || item.col !== col))
+  // const Tile = (props: any) => {
+  //   const { color, id, row, col, name } = props;
+  //   return (
+  //     <div
+  //       id={id}
+  //       className={isGridSelectable ? `${color} tile` : `nan tile`}
+  //       onMouseDown={() => {
+  //           if (isGridSelectable && isInList(manifest, row, col) && name !== "NAN" && name !== "UNUSED") {
+  //             if (isInList(selectedCells, row, col)) { // deselect cell
+  //               setSelectedCells(selectedCells.filter(item => item.row !== row || item.col !== col))
                 
-                let index = counts.findIndex((item => item.name === name));
-                let oldCount = counts[index].count;
-                if (oldCount == 1) { // last cell is deselected -> unhighlight all duplicate cells
-                  setSelectedNames(selectedNames.filter(item => item !== name))
-                  setCounts(counts.filter(item => item.name !== name))
-                } else {
-                  setCounts([...counts.slice(0, index)].concat(
-                            {...counts[index], count: oldCount - 1}, 
-                            [...counts.slice(index+1)]))
-                }
-              } else { // select cell
-                setSelectedCells([...selectedCells, { row, col }])
+  //               let index = counts.findIndex((item => item.name === name));
+  //               let oldCount = counts[index].count;
+  //               if (oldCount == 1) { // last cell is deselected -> unhighlight all duplicate cells
+  //                 setSelectedNames(selectedNames.filter(item => item !== name))
+  //                 setCounts(counts.filter(item => item.name !== name))
+  //               } else {
+  //                 setCounts([...counts.slice(0, index)].concat(
+  //                           {...counts[index], count: oldCount - 1}, 
+  //                           [...counts.slice(index+1)]))
+  //               }
+  //             } else { // select cell
+  //               setSelectedCells([...selectedCells, { row, col }])
   
-                if (!selectedNames.some((item: any) => item === name)) 
-                  setSelectedNames([...selectedNames, name])
+  //               if (!selectedNames.some((item: any) => item === name)) 
+  //                 setSelectedNames([...selectedNames, name])
                 
-                if (!counts.some((item: any) => item.name === name)) {
-                  setCounts([...counts, {name: name, count: 1}])
-                } else {
-                  let index = counts.findIndex((item => item.name === name))
-                  let oldCount = counts[index].count
-                  setCounts([...counts.slice(0, index)].concat(
-                            {...counts[index], count: oldCount + 1}, 
-                            [...counts.slice(index+1)]))
-                }
-              }
-          }
-        }}
-        style={{fontSize: "12px"}}
-      >
-        {name}
-      </div>
-    );
-  };
+  //               if (!counts.some((item: any) => item.name === name)) {
+  //                 setCounts([...counts, {name: name, count: 1}])
+  //               } else {
+  //                 let index = counts.findIndex((item => item.name === name))
+  //                 let oldCount = counts[index].count
+  //                 setCounts([...counts.slice(0, index)].concat(
+  //                           {...counts[index], count: oldCount + 1}, 
+  //                           [...counts.slice(index+1)]))
+  //               }
+  //             }
+  //         }
+  //       }}
+  //       style={{fontSize: "12px"}}
+  //     >
+  //       {name}
+  //     </div>
+  //   );
+  // };
 
-  const createBoard = (n: any, m: any) => {
-    const board = []
-    for (let row = n; row >= 1; row--) {
-      const boardRow = [];
-      for (let col = 1; col <= m; col++) {
-        let color = ""
-        const cell = manifest.find((item: any) => item.row === row && item.col === col) 
-        const name = cell ? cell.name : ""
-        if (isInList(manifest, row, col)) {
-          if (isInList(selectedCells, row, col)) 
-            color = "selected"
-          else if (selectedNames.some((item: any) => item === name))
-            color = "duplicate"
-          else if (name === "NAN") 
-            color = "nan"
-          else 
-            color = "unselected"
-        } else {
-          color = "nan"
-        }
+  // const createBoard = (n: any, m: any) => {
+  //   const board = []
+  //   for (let row = n; row >= 1; row--) {
+  //     const boardRow = [];
+  //     for (let col = 1; col <= m; col++) {
+  //       let color = ""
+  //       const cell = manifest.find((item: any) => item.row === row && item.col === col) 
+  //       const name = cell ? cell.name : ""
+  //       if (isInList(manifest, row, col)) {
+  //         if (isInList(selectedCells, row, col)) 
+  //           color = "selected"
+  //         else if (selectedNames.some((item: any) => item === name))
+  //           color = "duplicate"
+  //         else if (name === "NAN") 
+  //           color = "nan"
+  //         else 
+  //           color = "unselected"
+  //       } else {
+  //         color = "nan"
+  //       }
 
-        boardRow.push(
-          // <div
-          //   onContextMenu={(e) => {
-          //     e.preventDefault();
-          //     setClicked(true);
-          //     setPoints({
-          //       x: e.pageX,
-          //       y: e.pageY,
-          //     });
-          //   }}
-          // >
-            <Tile
-              key={`${col}${row}`}
-              color={color}
-              id={`${col},${row}`}
-              row={row}
-              col={col}
-              name={name}
-              // onContextMenu={(e: any) => {
-              //   e.preventDefault();
-              //   setClicked(true);
-              //   setPoints({
-              //     x: e.pageX,
-              //     y: e.pageY,
-              //   });
-              // }}
-            />
-          // </div>
-        );
-      }
-      board.push(
-        <div key={`${row}`} className="row">
-          {boardRow}
-        </div>
-      );
-    }
-    return board;
-  };
+  //       boardRow.push(
+  //         // <div
+  //         //   onContextMenu={(e) => {
+  //         //     e.preventDefault();
+  //         //     setClicked(true);
+  //         //     setPoints({
+  //         //       x: e.pageX,
+  //         //       y: e.pageY,
+  //         //     });
+  //         //   }}
+  //         // >
+  //           <Tile
+  //             key={`${col}${row}`}
+  //             color={color}
+  //             id={`${col},${row}`}
+  //             row={row}
+  //             col={col}
+  //             name={name}
+  //             // onContextMenu={(e: any) => {
+  //             //   e.preventDefault();
+  //             //   setClicked(true);
+  //             //   setPoints({
+  //             //     x: e.pageX,
+  //             //     y: e.pageY,
+  //             //   });
+  //             // }}
+  //           />
+  //         // </div>
+  //       );
+  //     }
+  //     board.push(
+  //       <div key={`${row}`} className="row">
+  //         {boardRow}
+  //       </div>
+  //     );
+  //   }
+  //   return board;
+  // };
 
   return (
     <div>
       <div className="split left">
         <div className="centered">
-          {createBoard(8, 12)}
+          {/* {createBoard(8, 12)} */}
+          <Grid 
+            manifest={manifest} 
+            n={8} 
+            m={12}
+            counts={counts}
+            setCounts={setCounts}
+            isGridSelectable={isGridSelectable}
+          />
           {/* {clicked && (
             <ContextMenu top={points.y} left={points.x}>
               <ul>
