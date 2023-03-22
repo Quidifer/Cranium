c526bc734cb348bf0bdfc2f0637dbb853c42e815
christopher mouri <christophermouri@gmail.com>
2023-03-22 02:05:04 -0700

    rebase

---
+++ client/src/components/ui/Calculating/Calculating.tsx
@@ -0,0 +1,44 @@
+import Video from "../../../resources/loadingbox.gif";
+import "../JobSelect/jobSelect.css";
+
+interface Props {
+  updateScreenState: () => void;
+}
+
+export default function Calculating(props: Props) {
+  setTimeout(() => {
+    props.updateScreenState();
+  }, 5000);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        width: "100vw",
+        height: "100vh",
+        backgroundColor: "#dfdfdf",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <img
+        style={{
+          height: "300px",
+          marginBottom: "50px",
+        }}
+        src={Video}
+        alt="Loading..."
+      />
+      <p
+        className="TopHeader"
+        style={{
+          marginLeft: "15px",
+          fontSize: "30px",
+        }}
+      >
+        Loading...
+      </p>
+    </div>
+  );
+}
--- client/src/components/ui/JobSelect/jobSelect.tsx
+++ client/src/components/ui/JobSelect/jobSelect.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import "./jobSelect.css";
-import JobSelectScreen from "../../../resources/JobSelectBackground.png";
+// import JobSelectScreen from "../../../resources/JobSelectBackground.png";
+import Crane from "../../../resources/flappingCrane.svg";
+import Balance from "../../../resources/balance.gif";
+import Truck from "../../../resources/truck.gif";
 
 interface Props {
   updateScreenState: (type: string) => void;
@@ -10,10 +13,10 @@ interface Props {
 export default function JobSelect(props: Props) {
   const { updateScreenState } = props;
   const [job, setJob] = useState<string>("");
-
   return (
     <div
       style={{
+        overflow: "hidden",
         position: "absolute",
         height: "100%",
         width: "100%",
@@ -22,20 +25,28 @@ export default function JobSelect(props: Props) {
         justifyContent: "center",
       }}
     >
-      <div className="TopHeader">Choose a Job</div>
       <div
         className="Background"
         style={{
           position: "absolute",
-          backgroundImage: `url(${JobSelectScreen})`,
           backgroundPosition: "center",
           backgroundSize: "cover",
           backgroundRepeat: "no-repeat",
           width: "100%",
           height: "100%",
-          opacity: "15%",
         }}
       ></div>
+      <img
+        src={Crane}
+        style={{
+          position: "absolute",
+          height: "100vh",
+          left: "calc(50% - 100vh/3)",
+          opacity: 0.3,
+        }}
+        alt=""
+      />
+      <div className="TopHeader">Choose a Job</div>
 
       <div className="MainContainer">
         <div className="Buttons">
@@ -45,6 +56,16 @@ export default function JobSelect(props: Props) {
               setJob("Load");
             }}
           >
+            <img
+              height="80px"
+              style={{
+                marginTop: "0px",
+                marginBottom: "15px",
+                marginLeft: "-30px",
+              }}
+              src={Truck}
+              alt={""}
+            />
             Load
           </button>
           <button
@@ -53,10 +74,17 @@ export default function JobSelect(props: Props) {
               setJob("Balance");
             }}
           >
+            <img
+              height="150px"
+              style={{ marginTop: "-30px", marginBottom: "-20px" }}
+              src={Balance}
+              alt={""}
+            />
             Balance
           </button>
           <div className="ContinueDiv">
             <button
+              disabled={job === ""}
               className="Continue"
               onClick={() => {
                 updateScreenState(job);
