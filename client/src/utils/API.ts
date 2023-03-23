80ca037e7909c5d6e4ba8462d979ef72d22fb43f
Tran Nguyen <tran28nguyen@gmail.com>
2023-03-22 21:23:43 -0700

    added Ryan's code

--- client/src/utils/API.ts
+++ client/src/utils/API.ts
@@ -1,13 +1,82 @@
+import { APISolution } from "../types/APISolution";
+
 export default class API {
-  public static async getLog() {
+
+  private static handleError(error: any) {
+    console.log(`API ERROR: ${error}`);
+  }
+
+  public static async sendJob(): Promise<boolean> {
+    let success: boolean = false;
+
+    return success;
+  }
+
+  public static async nextMove(): Promise<boolean> {
+    let success: boolean = false;
+
+    await fetch("/move")
+    .then((data) => {
+        console.log('200 - Move counter updated.');
+        success = (data.status === 200);
+      },
+      (reason) => API.handleError(reason)
+    );
+
+    return success;
+  }
+
+  public static async getSolution(): Promise<APISolution | null> {
+    let solution: APISolution | null = null;
+
+    await fetch("/solution")
+    .then(data => data.json())
+    .then((data) => {
+        solution = (data.body);
+      },
+      (reason) => API.handleError(reason)
+    );
+
+    return solution;
+  }
+
+  public static async getLog(): Promise<string> {
     let LogData: string = "ERROR";
 
     await fetch("/log").then(
-      async (data) => (LogData = await data.text()),
-      (reason) => (LogData = reason)
+      async (data) => LogData = await data.text(),
+      (reason) => API.handleError(reason)
     );
 
-    //return LogData;
-    return "LOG_DATA";
+    return LogData;
+  }
+
+  public static async sendLog(message: string, logType?: 'NONE' | 'SIGN_IN'): Promise<boolean> {
+    let success: boolean = false;
+
+    await fetch("/log", {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ message, logType})
+    }).then(
+      async (data) => {
+        console.log('200 - Log updated.');
+        success = (data.status === 200);
+      },
+      (reason) => API.handleError(reason)
+    );
+
+    return success;
+  }
+
+  public static async checkForSession(): Promise<boolean> {
+    let sessionExists: boolean = false;
+
+    await fetch("/check").then(
+      async (data) => sessionExists = (data.status === 200),
+      (reason) => API.handleError(reason)
+    );
+
+    return sessionExists;
   }
 }
